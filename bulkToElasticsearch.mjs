b084ae5f39e09f8be46413252130db55e66654e1
doorahmie <doorahmie@doorahmies-Mac-mini.local>
2024-04-08 17:44:20 +0900

    .crt ㅊㅜ가

--- bulkToElasticsearch.mjs
+++ bulkToElasticsearch.mjs
@@ -3,40 +3,47 @@ import { EventEmitter } from "events";
 import { v4 as uuidv4 } from "uuid"; // ES Modules
 import fs from "fs";
 // https://gemini.google.com/app/ad035f3411c57639
-const client = new Client({ node: "http://localhost:9200", log: "trace" });
+const client = new Client({
+  node: "http://localhost:9200",
+  log: "trace",
+  tls: {
+    ca: fs.readFileSync("http_ca.crt"),
+    rejectUnauthorized: false,
+  },
+});
 const indexName = "hanja-20240401";
 const testHanjaData = JSON.parse(fs.readFileSync("testHanjaData.json"));
 const eEmitter = new EventEmitter();
 
 // const newDocArray = [];
-for (let key in testHanjaData) {
-  // console.log(key); // 프로퍼티 이름 출력
-  // const item = { value: testHanjaData[key] };
-  let eachItemWithKey = {};
-  testHanjaData[key].map((item) => {
-    // console.log(item);
-    eachItemWithKey = { ...item, key };
-    console.log(eachItemWithKey);
-    const id = uuidv4();
-    fs.writeFile("");
-    /**
-     * text 파일로 값 생성한뒤
-     * 한줄 씩 밀어넣는 bulk로 진행
-     */
-    // client.index(
-    //   {
-    //     index: indexName,
-    //     body: eachItemWithKey,
-    //   },
-    //   (err, result) => {
-    //     if (err) {
-    //       console.log(err);
-    //     }
-    //     console.log("저장 성공: ", result);
-    //   }
-    // );
-  });
-}
+// for (let key in testHanjaData) {
+//   // console.log(key); // 프로퍼티 이름 출력
+//   // const item = { value: testHanjaData[key] };
+//   let eachItemWithKey = {};
+//   testHanjaData[key].map((item) => {
+//     // console.log(item);
+//     eachItemWithKey = { ...item, key };
+//     console.log(eachItemWithKey);
+//     const id = uuidv4();
+//     fs.writeFile("");
+//     /**
+//      * text 파일로 값 생성한뒤
+//      * 한줄 씩 밀어넣는 bulk로 진행
+//      */
+//     // client.index(
+//     //   {
+//     //     index: indexName,
+//     //     body: eachItemWithKey,
+//     //   },
+//     //   (err, result) => {
+//     //     if (err) {
+//     //       console.log(err);
+//     //     }
+//     //     console.log("저장 성공: ", result);
+//     //   }
+//     // );
+//   });
+// }
 
 async function bootstrap() {
   /*https://velog.io/@alli-eunbi/Elastic-Search-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-with-NodeJS*/
